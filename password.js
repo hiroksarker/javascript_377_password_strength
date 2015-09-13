7b37a5dc78dd13d15c8049320509d04252a1a9e9
hiroksarker <[hirok.sarker@gmail.com>
2015-09-13 23:31:05 +0600

    JS Change
    JS Change

--- password.js
+++ password.js
@@ -1,32 +1,45 @@
 $(document).ready(function () {
+    'use strict';
+
     $('#password').keyup(function () {
-        $('#result').html(checkStrength($('#password').val()))
-    })
+        $('#result').html(checkStrength($('#password').val()));
+    });
 
     function checkStrength(password) {
-        var strength = 0
+        var strength = 0;
         if (password.length < 6) {
-            $('#result').removeClass()
-            $('#result').addClass('short')
-            return 'Too short'
+            $('#result').removeClass();
+            $('#result').addClass('short');
+            return 'Too short';
         }
-        if (password.length > 7) strength += 1
-        if (password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)) strength += 1
-        if (password.match(/([a-zA-Z])/) && password.match(/([0-9])/)) strength += 1
-        if (password.match(/([!,%,&,@,#,$,^,*,?,_,~])/)) strength += 1
-        if (password.match(/(.*[!,%,&,@,#,$,^,*,?,_,~].*[!,%,&,@,#,$,^,*,?,_,~])/)) strength += 1
+        if (password.length > 7) {
+            strength += 1;
+        }
+        if (password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)) {
+            strength += 1;
+        }
+        if (password.match(/([a-zA-Z])/) && password.match(/([0-9])/)) {
+            strength += 1;
+        }
+        if (password.match(/([!,%,&,@,#,$,^,*,?,_,~])/)) {
+            strength += 1;
+        }
+        if (password.match(/(.*[!,%,&,@,#,$,^,*,?,_,~].*[!,%,&,@,#,$,^,*,?,_,~])/)) {
+            strength += 1;
+        }
         if (strength < 2) {
-            $('#result').removeClass()
-            $('#result').addClass('weak')
-            return 'Weak'
+            $('#result').removeClass();
+            $('#result').addClass('weak');
+            return 'Weak';
         } else if (strength == 2) {
-            $('#result').removeClass()
-            $('#result').addClass('good')
-            return 'Good'
+            $('#result').removeClass();
+            $('#result').addClass('good');
+            return 'Good';
         } else {
-            $('#result').removeClass()
-            $('#result').addClass('strong')
-            return 'Strong'
+            $('#result').removeClass();
+            $('#result').addClass('strong');
+            return 'Strong';
         }
     }
+    checkStrength();
 });
